3e3cc321c7a9010062327649d7bc018493605a59
Patrick Mineault <patrick.mineault@gmail.com>
2017-06-04 16:15:58 -0700

    Remove dependency on deprecated seq2seq lib.

--- model.py
+++ model.py
@@ -3,6 +3,53 @@ import tensorflow as tf
 import numpy as np
 import random
 
+def rnn_decoder(decoder_inputs,
+                initial_state,
+                cell,
+                loop_function=None,
+                scope=None):
+  """RNN decoder for the sequence-to-sequence model.
+  Args:
+    decoder_inputs: A list of 2D Tensors [batch_size x input_size].
+    initial_state: 2D Tensor with shape [batch_size x cell.state_size].
+    cell: rnn_cell.RNNCell defining the cell function and size.
+    loop_function: If not None, this function will be applied to the i-th output
+      in order to generate the i+1-st input, and decoder_inputs will be ignored,
+      except for the first element ("GO" symbol). This can be used for decoding,
+      but also for training to emulate http://arxiv.org/abs/1506.03099.
+      Signature -- loop_function(prev, i) = next
+        * prev is a 2D Tensor of shape [batch_size x output_size],
+        * i is an integer, the step number (when advanced control is needed),
+        * next is a 2D Tensor of shape [batch_size x input_size].
+    scope: VariableScope for the created subgraph; defaults to "rnn_decoder".
+  Returns:
+    A tuple of the form (outputs, state), where:
+      outputs: A list of the same length as decoder_inputs of 2D Tensors with
+        shape [batch_size x output_size] containing generated outputs.
+      state: The state of each cell at the final time-step.
+        It is a 2D Tensor of shape [batch_size x cell.state_size].
+        (Note that in some cases, like basic RNN cell or GRU cell, outputs and
+         states can be the same. They are different for LSTM cells though.)
+  """
+  with tf.variable_scope(scope or "rnn_decoder"):
+    state = initial_state
+    outputs = []
+    prev = None
+    states = []
+    for i, inp in enumerate(decoder_inputs):
+      if loop_function is not None and prev is not None:
+        with tf.variable_scope("loop_function", reuse=True):
+          inp = loop_function(prev, i)
+      if i > 0:
+        tf.get_variable_scope().reuse_variables()
+      output, state = cell(inp, state)
+      states.append(state)
+      outputs.append(output)
+      if loop_function is not None:
+        prev = output
+  return outputs, state, states
+
+
 class Model():
   def __init__(self, args, infer=False):
     self.args = args
@@ -45,10 +92,13 @@ class Model():
     # inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
     inputs = tf.unstack(self.input_data, axis=1)
 
-    outputs, state_out = tf.contrib.legacy_seq2seq.rnn_decoder(inputs, self.state_in, cell, loop_function=None, scope='rnnlm')
+    outputs, state_out, all_states = rnn_decoder(inputs, self.state_in, cell, loop_function=None, scope='rnnlm')
     output = tf.reshape(tf.concat(axis=1, values=outputs), [-1, args.rnn_size])
     output = tf.nn.xw_plus_b(output, output_w, output_b)
     self.state_out = tf.identity(state_out, name='state_out')
+
+    self.all_states = tf.stack(all_states, axis=1)
+    self.state_mean, self.state_var = tf.nn.moments(self.all_states, axes=[1])
 
     # reshape target data so that it is compatible with prediction shape
     flat_target_data = tf.reshape(self.target_data,[-1, 3])
